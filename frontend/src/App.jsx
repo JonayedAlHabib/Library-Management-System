9dc91b2bd8e58e576f16ca834553b3389759618b
Jonayed Al Habib <jonayedalhabibm135@gmail.com>
2026-06-30 17:29:18 +0600

    Update App.jsx with all routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,9 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import AdminDashboard from './pages/admin/Dashboard';
+import AdminBooks from './pages/admin/Books';
 import UserDashboard from './pages/user/Dashboard';
+import UserBooks from './pages/user/Books';
 
 export default function App() {
   return (
@@ -12,8 +14,14 @@ export default function App() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      
+      {/* Admin */}
       <Route path="/admin/dashboard" element={<AdminDashboard />} />
+      <Route path="/admin/books" element={<AdminBooks />} />
+      
+      {/* User */}
       <Route path="/user/dashboard" element={<UserDashboard />} />
+      <Route path="/user/books" element={<UserBooks />} />
     </Routes>
   );
 }
